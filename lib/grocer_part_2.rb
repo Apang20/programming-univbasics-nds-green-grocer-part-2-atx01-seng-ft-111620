8364c38d4590ae1ff435c58889b7cc2797c6eca6
Avelon Pang <avelonpang@gmail.com>
2020-11-09 22:32:38 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -29,7 +29,21 @@ require 'pry'
 # cart 
 # end 
 
+def apply_coupons(cart, coupons)
+  # code here	coupons.each do |coupon|
+  name = coupon[:item] #makes things easier
 
+  if cart[name] && cart[name][:count] >= coupon[:num]
+    if cart["#{name} W/COUPON"]
+      cart["#{name} W/COUPON"][:count] += coupon[:num]
+    else
+      cart["#{name} W/COUPON"] = {:price => coupon[:cost]/coupon[:num], :clearance => cart[name][:clearance], :count => coupon[:num]}
+    end
+  cart[name][:count] -= coupon[:num]
+  end
+end
+cart
+end	end
 
 
 
