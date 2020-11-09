4b2363eefc7963f953b25447d323ca6a27288df1
Avelon Pang <avelonpang@gmail.com>
2020-11-09 23:45:47 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -46,5 +46,9 @@ couponed_cart = apply_coupons(consolidated_cart)
 final_cart = apply_clearance(couponed_cart)
 
 total = 0 
+counter = 0 
+while counter < final_cart.length 
 
+counter += 1 
+end 
 end
