12e401da2321d6e19b416942c08e5e3f241cefde
Avelon Pang <avelonpang@gmail.com>
2020-11-09 23:49:03 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -48,8 +48,8 @@ final_cart = apply_clearance(couponed_cart)
 total = 0 
 counter = 0 
 while counter < final_cart.length 
-total += final_cart[counter][:price] * final_cart[counter][:count]
-counter += 1 
+   total += final_cart[counter][:price] * final_cart[counter][:count]
+   counter += 1 
 end 
 if total > 100
   total -= (total - 0.10)
