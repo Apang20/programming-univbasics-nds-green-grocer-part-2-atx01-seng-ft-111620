dd18d7cf4ed025ffd3a3561c0e69a35a018e3d29
Avelon Pang <avelonpang@gmail.com>
2020-11-09 23:04:10 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -27,25 +27,6 @@ cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart
 cart 
 end 
 
-# def apply_coupons(cart, coupons)
-#   coupons.each do |coupon|
-#     name = coupon[:item]
-#     if cart[name] && cart[name][:count] >= coupon[:num]
-#       if cart["#{name} W/COUPON"]
-#         cart["#{name} W/COUPON"][:count] += 1
-#       else
-#         cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
-#         cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
-#       end
-#       cart[name][:count] -= coupon[:num]
-#     end
-#   end
-#   cart
-# end
-
-
-
-
 
 def apply_clearance(cart)
  
